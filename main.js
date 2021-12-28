35875e09a8cf9c656326ba1a8a67a5a936e52316
blueisis <pureun96@naver.com>
2021-12-28 20:55:10 +0900

    es5 콜백함수를 통한 동기화 방법

--- main.js
+++ main.js
@@ -4,29 +4,28 @@ function test(delay, callback){
   },delay);
 }
 
+console.log("start");
+
 test(1000, function(){
   console.log("callback1");
 })
 
+test(1000, function(){
+  console.log("callback2");
+})
+
 /*
 1. "start" 실행 -js
-2. 첫번째 test()실행 -js
-3. test 함수 안쪽의 setTimeout -web api 전달
-4. 두번째 test()실행 -js
-5. test 함수 안쪽의 setTimeout -web api 전달
-6. 세번째 test()실행 -js
-7. test 함수 안쪽의 setTimeout -web api 전달
-8. console.log("end")실행 -js
-9. 1초 뒤에 setTimeout 실행하고 "callback1" -callstak에 전달 -web
-10. 1초 뒤에 setTimeout 실행하고 "callback2" -callstak에 전달 -web
-11. 1초 뒤에 setTimeout 실행하고 "callback3" -callstak에 전달 -web
+2. 첫번째 test() 함수 실행  -js
+3. 첫번째 test() 함수 안쪽의 setTimeout을 web api에게 전달
+4. 2초뒤 setTimeout 실행 -web api
+5. console.log("callback1"), 두번째 test()함수를 callstack에 전달
+6. callstack이 "callback1" 실행, 두번째 test() 함수 실행.
+7. 두번째 test() 함수 안쪽의 setTimeout을 web api에게 전달 
+위의 단계 반복
 
-출력
-
-start
-end
-callback1
-callback2
-callback3
-
-*/ 
+콜백을 통한 동기화 방식의 원리는
+순차적으로 실행할 모든 함수들을 하나의 callback함수에 넣어서
+콜스택이 다음에 실행할 함수가 없도록 만듬
+단점 - 코드의 뎁스가 깊어지면서 지저분해짐 (callback hell)
+*/
